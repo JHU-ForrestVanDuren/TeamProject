c56fb91a2f9ba51791b9289c082085297ede086e
root <root@rpi-buster-armhf>
2025-04-26 22:27:07 +0000

    Adds support for exponentation and modulo for numbers over 32 bits large

--- RSALib.s
+++ RSALib.s
@@ -8,6 +8,7 @@
 .global gcd
 .global pow
 .global modulo
+.global modLarge
 .global cpubexp
 .global cprivexp
 .global encrypt
@@ -44,7 +45,7 @@ gcd:
     #while b>0
     beginLoop:
     CMP r5, #0
-    BLE endLoop
+    BLE endGCDLoop
         #r0 = tmp
         #tmp = b
         MOV r0, r5
@@ -63,7 +64,7 @@ gcd:
         MOV r4, r0
         B beginLoop
 
-    endLoop:
+    endGCDLoop:
     MOV r0, r4
 
     #Pop the stack
@@ -76,53 +77,140 @@ gcd:
 #END FUNCTION gcd
 
 #
-#pow
-#Section 81 Team 5
-#Takes in two numbers r0 and r1 and performs exponentiation r0 ^ r1
-
+#Function Name: pow
+#Authors:       Quinn Waxter, Forrest VanDuren
+#Purpose:       To perform exponentiation on two numbers. Specifically for numbers that will be much larger then 32 bits long
+#
 .text
 pow:
-    # r4 stores the base number
-    # r5 stores power
-    # r6 holds value containing number of loop iterations remaining
+	# r4 stores the base number
+	# r5 stores the product of the current power multiplication for each index
+	# r6 holds value containing number of loop iterations remaining
+	# r7 number to multiply index by to get to the appropriate memory location
+	# r8 stores the memory location offset for numArray
+	# r9 stores the inner loop counter
+	# r10 Stores the number of indices for numArray
+	# r11 Stores the carry value for each iteration
 
-    #push stack
-    SUB sp, sp, #16
-    STR lr, [sp]
-    STR r4, [sp, #4]
-    STR r5, [sp, #8]
-    STR r6, [sp, #12]
+	#push stack
+	SUB sp, sp, #36
+	STR lr, [sp]
+	STR r4, [sp, #4]
+	STR r5, [sp, #8]
+	STR r6, [sp, #12]
+	STR r7, [sp, #16]
+	STR r8, [sp, #20]
+	STR r9, [sp, #24]
+	STR r10, [sp, #28]
+	STR r11, [sp, #32]
 
-    # Store Values needed for Later
-    MOV r4, r0
-    MOV r5, r1
+	# Store Values needed for Later
+	MOV r4, r0
+	MOV r6, r1
+	MOV r7, #4
+	MOV r9, #0
+	MOV r10, #0
 
-    # Check if exponent is 0
-    CMP r1, #0
-    MOVEQ r0, #1
-    BEQ return
+	# Check if exponent is 0
+	CMP r1, #0
+	MOVEQ r0, #1
+	BEQ return
    
-    # Run Loop
-    MOV r0, #1
-    MOV r6, r5
+	# Stores initial base value in numArray
+	LDR r0, =numArray
+	STR r4, [r0]
 
-    multiplyLoop:
-        CMP r6, #0
-            BEQ return
-        MUL r0, r0, r4
-        SUB r6, r6, #1
-        B multiplyLoop
+	#Run loop
+	multiplyLoop:
 
-    return:
+	
+		CMP r6, #1
+		BEQ endMultiplyLoop
+		MOV r8, #0
+		MOV r11, #0
+	
+		innerloop:
+
+                        CMP r9, r10
+                        BGT endinnerloop
+
+			#Get numArray value at current index
+                        MUL r8, r9, r7
+                        LDR r1, [r0, r8]
+
+			#Multiply base number and add cary from last index
+                        MUL r5, r4, r1
+                        ADD r5, r5, r11
+
+			#Increase current index and compare r5 to the maximum number allowed before carrying. Branch to greater then if over this amount.
+                        ADD r9, r9, #1
+                        LDR r3, =#65535
+                        CMP r5, r3
+                        BGT greaterthan
+
+				#Store new value for the current index in the array and iterate innerloop
+                                STR r5, [r0, r8]
+				MOV r11, #0
+                                B innerloop
+
+                        greaterthan:
+
+				#Generate carry value	
+                                MOV r11, r5
+                                LSR r11, r11, #16
+
+				#Remove carry value from the front of the current numArray index value
+                                LDR r3, =#0x0000FFFF
+                                AND r5, r5, r3
+
+				#If r9 is greater then the current highest index branch to extend the array
+                                CMP r9, r10
+                                BGT extendarray
+
+					#Store new value of current index
+                                	STR r5, [r0, r8]
+                                	B innerloop
+
+                                extendarray:
+
+					#Store new value of current index, add 4 to offset and store carry into that new offset
+                                        STR r5, [r0, r8]
+                                        ADD r8, r8, #4
+                                        STR r11, [r0, r8]
+
+					#Increase highest index number and end innerloop
+                                        ADD r10, r10, #1
+                                        B endinnerloop
+
+                endinnerloop:
+
+		#Reset inner loop counter, decrease outerloop counter, and branch back to the begining of the outerloop
+                MOV r9, #0
+                SUB r6, r6, #1
+                B multiplyLoop
+
+	endMultiplyLoop:
+
+	return:
+	
+	#Return pointer to numArray in r0 and the size of the array in r1
+	MOV r1, r10
+
         # return stack
         LDR lr, [sp]
 	LDR r4, [sp, #4]
 	LDR r5, [sp, #8]
 	LDR r6, [sp, #12]
-        ADD sp, sp, #16
+	LDR r7, [sp, #16]
+	LDR r8, [sp, #20]
+	LDR r9, [sp, #24]
+	LDR r10, [sp, #28]
+	LDR r11, [sp, #32]
+        ADD sp, sp, #36
         MOV pc, lr
 
 .data
+    numArray: .word 0
 #END FUNCTION pow
 
 #
@@ -158,6 +246,81 @@ modulo:
 .data
 #END FUNCTION modulo
 
+#
+#Function Name: modLarge
+#Auther:        Forrest VanDuren
+#Date:          04/26/2025
+#Purpose:       To create a function that's capable of finding the modulo of number much larger then 32 bits long
+#
+.text
+modLarge:
+
+	# r4 holds the divisor
+	# r5 holds the dividend of the current array index
+	# r6 holds the current index remainder
+	# r7 holds the number to multiply the index by which gives the current array offset
+	# r8 holds the current array index
+	# r9 holds a pointer to the number array
+
+	#Push the stack record
+	Sub sp, sp, #28
+	STR lr, [sp]
+	STR r4, [sp, #4]
+	STR r5, [sp, #8]
+	STR r6, [sp, #12]
+	STR r7, [sp, #16]
+	STR r8, [sp, #20]
+	STR r9, [sp, #24]
+
+	#Initialize values
+	MOV r4, r2
+	MOV r6, #0
+	MOV r7, #4
+	MOV r8, r1
+	MOV r9, r0
+
+        modloop:
+
+                CMP r8, #0
+                BLT endmodloop
+
+		#Get value of current index
+                MUL r1, r7, r8
+                LDR r0, [r9, r1]
+
+		#Shift last iteration remainder into first 16 bits and combine with current index value to get this iterations dividend
+                LSL r6, #16
+                ORR r0, r0, r6
+
+		#Store dividend in r5, move divisor to r1, and divide
+                MOV r5, r0
+                MOV r1, r4
+                BL __aeabi_idiv
+
+		#Calculate the remainder, decrease the array index and iterate the loop
+                MUL r1, r0, r4
+                SUB r6, r5, r1
+                SUB r8, r8, #1
+                B modloop
+
+        endmodloop:
+	
+	#Return final remainder in r0
+	MOV r0, r6
+
+	#Pop the stack record
+	LDR lr, [sp,#0]
+	LDR r4, [sp, #4]
+	LDR r5, [sp, #8]
+	LDR r6, [sp, #12]
+	LDR r7, [sp, #16]
+	LDR r8, [sp, #20]
+	LDR r9, [sp, #24]
+	ADD sp, sp, #28
+	MOV pc, lr
+
+.data
+#END FUNCTION modLarge
 #
 #Function Name: cpubexp
 #Author:        Cory Drangel
@@ -335,7 +498,7 @@ decrypt:
         MOV r0, r7
         BL fgetc
         CMP r0, #-1  // must call fgetc before determining EOF
-        BEQ endLoop
+        BEQ endDecryptLoop
 
         MOV r1, r4 // move 'd' to r1 for POW function
         BL pow
@@ -348,7 +511,7 @@ decrypt:
 
         B decryptLoop
 
-    endLoop:
+    endDecryptLoop:
         MOV r1, r7
         BL fclose
     
